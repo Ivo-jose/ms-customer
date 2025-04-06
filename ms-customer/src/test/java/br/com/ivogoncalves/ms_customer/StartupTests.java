555dc5761422018f8773b0dfcf67e8869ef9cc62
Ivo Jose <ivojmgs19@gmail.com>
2025-04-06 15:28:23 -0300

    Project Customer created

---
+++ ms-customer/src/test/java/br/com/ivogoncalves/ms_customer/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.ivogoncalves.ms_customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
