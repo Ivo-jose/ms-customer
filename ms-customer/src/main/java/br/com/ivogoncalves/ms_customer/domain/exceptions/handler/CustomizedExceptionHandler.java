dcf833fea2044e7a90f2b1a619108d6606dafe5a
Ivo Goncalves <ivojmgs19@gmail.com>
2025-05-12 19:01:11 -0300

    Eliminating unnecessary coding

--- ms-customer/src/main/java/br/com/ivogoncalves/ms_customer/domain/exceptions/handler/CustomizedExceptionHandler.java
+++ ms-customer/src/main/java/br/com/ivogoncalves/ms_customer/domain/exceptions/handler/CustomizedExceptionHandler.java
@@ -10,11 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import br.com.ivogoncalves.ms_customer.domain.exceptions.AttributeValidationException;
-import br.com.ivogoncalves.ms_customer.domain.exceptions.DataIntegrityViolationException;
 import br.com.ivogoncalves.ms_customer.domain.exceptions.ExceptionResponse;
 import br.com.ivogoncalves.ms_customer.domain.exceptions.ResourceNotFoundException;
-import jakarta.validation.ConstraintViolationException;
 
 /**
  * CustomizedExceptionHandler
@@ -37,30 +34,5 @@ public class CustomizedExceptionHandler extends ResponseEntityExceptionHandler {
 	public ResponseEntity<ExceptionResponse> handleNotFoundException(Exception ex, WebRequest request) {
 		ExceptionResponse exception = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(true));
 		return new ResponseEntity<ExceptionResponse>(exception,HttpStatus.NOT_FOUND);
-	}
-	
-	@ExceptionHandler(AttributeValidationException.class)
-	public ResponseEntity<ExceptionResponse> handleAttributeValidationException(Exception ex, WebRequest request) {
-		ExceptionResponse exception = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(true));
-		return new ResponseEntity<ExceptionResponse>(exception,HttpStatus.BAD_REQUEST);
-	}
-	
-	@ExceptionHandler(DataIntegrityViolationException.class)
-	public ResponseEntity<ExceptionResponse> handleDataIntegrityViolationException(Exception ex, WebRequest request) {
-		ExceptionResponse exception = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(true));
-		return new ResponseEntity<ExceptionResponse>(exception,HttpStatus.BAD_REQUEST);
-	}
-	
-	@ExceptionHandler(ConstraintViolationException.class)
-	public ResponseEntity<ExceptionResponse> handleConstraintViolationException(
-	        ConstraintViolationException ex, WebRequest request) {
-
-	    StringBuilder sb = new StringBuilder("VALIDATION ERROR: ");
-	    ex.getConstraintViolations().forEach(violation -> {
-	        sb.append(violation.getPropertyPath()).append(": ");
-	        sb.append(violation.getMessage()).append("; ");
-	    });
-	    ExceptionResponse exception = new ExceptionResponse(new Date(), sb.toString(), request.getDescription(true));
-	    return new ResponseEntity<>(exception, HttpStatus.BAD_REQUEST);
 	}
 }
